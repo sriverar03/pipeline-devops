f9b3f57b75a2942e1c3daf3dd78ed19d1fb2df24
sriverar03 <95256304+sriverar03@users.noreply.github.com>
2022-01-30 19:29:15 -0300

    Update gradle.groovy

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -6,38 +6,39 @@
 
 def call(){
 	
-	/*String[] str
+	String[] str
       	str = params.stage.split(';')
-  	println str[1]*/
+  	println str.length()
 	
-	
-	stage('TestBuild') {            
-		bat "gradle Build"
-	}
-	
-	stage('SonarQube analysis') {            
-                    def scannerHome = tool 'sonar-scanner';
-                    withSonarQubeEnv('sonar-server') { 
-                    bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.sources=src -Dsonar.java.binaries=build " 
-                    }           
-        }
-	
-	stage('Run') {           
-                bat "gradle bootRun "           
-        }
-	
-	stage('Nexus') {            
-                bat "curl -v --user admin:123456 --upload-file C:/Users/nmt02/.jenkins/workspace/pipilene_sonar_feature-sonar/build/DevOpsUsach2020-0.0.1.jar http://7fb6-186-79-184-102.ngrok.io/repository/test-repo/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar "            
-        }        
-        
-        
-        
+	if(str.length() != 0)
+	{	
+		stage('TestBuild') {            
+			bat "gradle Build"
+		}
 
-        
-        
-        /*stage('Test Application') {           
-                    bat "curl -X GET http://localhost:8081/rest/mscovid/test?msg=testing"             
-        }*/
+		stage('SonarQube analysis') {            
+			    def scannerHome = tool 'sonar-scanner';
+			    withSonarQubeEnv('sonar-server') { 
+			    bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.sources=src -Dsonar.java.binaries=build " 
+			    }           
+		}
+
+		stage('Run') {           
+			bat "gradle bootRun "           
+		}
+
+		stage('Nexus') {            
+			bat "curl -v --user admin:123456 --upload-file C:/Users/nmt02/.jenkins/workspace/pipilene_sonar_feature-sonar/build/DevOpsUsach2020-0.0.1.jar http://7fb6-186-79-184-102.ngrok.io/repository/test-repo/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar "            
+		} 
+
+		/*stage('Test Application') {           
+			    bat "curl -X GET http://localhost:8081/rest/mscovid/test?msg=testing"             
+		}*/
+	}
+	else
+	{
+		println 'stage vacio'
+	}
         
         
 
