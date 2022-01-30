445d4488cc8622134316400e8031e271403fde19
sriverar03 <95256304+sriverar03@users.noreply.github.com>
2022-01-30 19:11:42 -0300

    Update gradle.groovy

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -13,30 +13,10 @@ def call(){
 	
 	
 	
-	if(str[0] == 'build'){	
-		build()
-	}
-	
-	if(params.stage == 'sonar'){	
-		stage('SonarQube analysis') {            
-                    def scannerHome = tool 'sonar-scanner';
-                    withSonarQubeEnv('sonar-server') { 
-                    bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.sources=src -Dsonar.java.binaries=build " 
-                    }           
-        	}
-	}
-	
-	if(params.stage == 'run'){	
-		 stage('Run') {           
-                    bat "gradle bootRun "           
-        	}	
-	}
-	
-	if(params.stage == 'nexus'){	
-		stage('Nexus') {            
-                    bat "curl -v --user admin:123456 --upload-file C:/Users/nmt02/.jenkins/workspace/pipilene_sonar_feature-sonar/build/DevOpsUsach2020-0.0.1.jar http://7fb6-186-79-184-102.ngrok.io/repository/test-repo/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar "            
-        	}
-	}
+	build()
+	sonar()
+	run()
+	nexus()
 	
         
         
@@ -54,9 +34,30 @@ def call(){
 }
 
 def build(){
-		stage('TestBuild') {            
-			bat "gradle Build"
-		}
+	stage('TestBuild') {            
+		bat "gradle Build"
 	}
+}
+
+def (sonar){
+	stage('SonarQube analysis') {            
+                    def scannerHome = tool 'sonar-scanner';
+                    withSonarQubeEnv('sonar-server') { 
+                    bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.sources=src -Dsonar.java.binaries=build " 
+                    }           
+        }
+}
+
+def run(){
+	stage('Run') {           
+                bat "gradle bootRun "           
+        }
+}
+
+def nexus(){
+	stage('Nexus') {            
+                bat "curl -v --user admin:123456 --upload-file C:/Users/nmt02/.jenkins/workspace/pipilene_sonar_feature-sonar/build/DevOpsUsach2020-0.0.1.jar http://7fb6-186-79-184-102.ngrok.io/repository/test-repo/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar "            
+        }
+}
 
 return this;
