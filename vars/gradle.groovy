90672955b7e97ef1a6b0e0772c4760d3ca7ee487
sriverar03 <95256304+sriverar03@users.noreply.github.com>
2022-01-30 19:41:56 -0300

    Update gradle.groovy

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -12,29 +12,39 @@ def call(){
 	
 	println str.contains('test')
 	
-	/*if(str.size() != 0)
+	if(str.contains('build'))
 	{	
 		stage('TestBuild') {            
 			bat "gradle Build"
 		}
-
+	}
+	
+	if(str.contains('sonar'))
+	{
 		stage('SonarQube analysis') {            
 			    def scannerHome = tool 'sonar-scanner';
 			    withSonarQubeEnv('sonar-server') { 
 			    bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.sources=src -Dsonar.java.binaries=build " 
 			    }           
 		}
-
+	}
+	
+	if(str.contains('run'))
+	{
 		stage('Run') {           
 			bat "gradle bootRun "           
 		}
-
+	}
+	
+	if(str.contains('nexus'))
+	{
 		stage('Nexus') {            
 			bat "curl -v --user admin:123456 --upload-file C:/Users/nmt02/.jenkins/workspace/pipilene_sonar_feature-sonar/build/DevOpsUsach2020-0.0.1.jar http://7fb6-186-79-184-102.ngrok.io/repository/test-repo/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar "            
 		} 
+	}
 
 		
-	}*/
+	
 	
 	/*stage('Test Application') {           
 			    bat "curl -X GET http://localhost:8081/rest/mscovid/test?msg=testing"             
