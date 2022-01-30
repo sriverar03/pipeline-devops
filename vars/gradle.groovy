f1037e013363080984b9295c446df66307a7590b
sriverar03 <95256304+sriverar03@users.noreply.github.com>
2022-01-30 19:17:54 -0300

    Update gradle.groovy

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -11,14 +11,24 @@ def call(){
   	println str[1]
 	
 	
+	stage('TestBuild') {            
+		bat "gradle Build"
+	}
 	
+	stage('SonarQube analysis') {            
+                    def scannerHome = tool 'sonar-scanner';
+                    withSonarQubeEnv('sonar-server') { 
+                    bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.sources=src -Dsonar.java.binaries=build " 
+                    }           
+        }
 	
-	build()
-	sonar()
-	run01()
-	nexus()
+	stage('Run') {           
+                bat "gradle bootRun "           
+        }
 	
-        
+	stage('Nexus') {            
+                bat "curl -v --user admin:123456 --upload-file C:/Users/nmt02/.jenkins/workspace/pipilene_sonar_feature-sonar/build/DevOpsUsach2020-0.0.1.jar http://7fb6-186-79-184-102.ngrok.io/repository/test-repo/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar "            
+        }        
         
         
         
@@ -33,31 +43,6 @@ def call(){
 
 }
 
-def build(){
-	stage('TestBuild') {            
-		bat "gradle Build"
-	}
-}
 
-def sonar(){
-	stage('SonarQube analysis') {            
-                    def scannerHome = tool 'sonar-scanner';
-                    withSonarQubeEnv('sonar-server') { 
-                    bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.sources=src -Dsonar.java.binaries=build " 
-                    }           
-        }
-}
-
-def run01(){
-	stage('Run') {           
-                bat "gradle bootRun "           
-        }
-}
-
-def nexus(){
-	stage('Nexus') {            
-                bat "curl -v --user admin:123456 --upload-file C:/Users/nmt02/.jenkins/workspace/pipilene_sonar_feature-sonar/build/DevOpsUsach2020-0.0.1.jar http://7fb6-186-79-184-102.ngrok.io/repository/test-repo/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar "            
-        }
-}
 
 return this;
