1baac823e83a1d34ae9637b78800f823cd9270de
sriverar03 <95256304+sriverar03@users.noreply.github.com>
2022-01-30 20:28:59 -0300

    Update gradle.groovy

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -13,54 +13,58 @@ def call(){
 	
 	//println str.contains('test')
 	
-	def bandera = false
+	def bandera = true
 	for (int i = 0; i < str.size(); i++) {
 		for (int n = 0; n < stages.size(); n++) {
 			if(str[i] != stages[n]){
-			 bandera = true
+			 bandera = false
 			}
 		}		
 	}
 	
 	println bandera
-	
-	if(str.contains('build') || params.stage.isEmpty() )
-	{	
-		stage('TestBuild') {            
-			bat "gradle Build"
+	if(bandera){
+		if(str.contains('build') || params.stage.isEmpty() )
+		{	
+			stage('TestBuild') {            
+				bat "gradle Build"
+			}
 		}
-	}
-	
-	if(str.contains('sonar') || params.stage.isEmpty())
-	{
-		stage('SonarQube analysis') {            
-			    def scannerHome = tool 'sonar-scanner';
-			    withSonarQubeEnv('sonar-server') { 
-			    bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.sources=src -Dsonar.java.binaries=build " 
-			    }           
-		}
-	}
-	
-	if(str.contains('run') || params.stage.isEmpty())
-	{
-		stage('Run') {           
-			bat "gradle bootRun "           
-		}
-	}
-	
-	if(str.contains('nexus') || params.stage.isEmpty())
-	{
-		stage('Nexus') {            
-			bat "curl -v --user admin:123456 --upload-file C:/Users/nmt02/.jenkins/workspace/pipilene_sonar_feature-sonar/build/DevOpsUsach2020-0.0.1.jar http://7fb6-186-79-184-102.ngrok.io/repository/test-repo/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar "            
-		} 
-	}
 
-		
-	
-	
-	/*stage('Test Application') {           
-			    bat "curl -X GET http://localhost:8081/rest/mscovid/test?msg=testing"             
-	}*/
+		if(str.contains('sonar') || params.stage.isEmpty())
+		{
+			stage('SonarQube analysis') {            
+				    def scannerHome = tool 'sonar-scanner';
+				    withSonarQubeEnv('sonar-server') { 
+				    bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.sources=src -Dsonar.java.binaries=build " 
+				    }           
+			}
+		}
+
+		if(str.contains('run') || params.stage.isEmpty())
+		{
+			stage('Run') {           
+				bat "gradle bootRun "           
+			}
+		}
+
+		if(str.contains('nexus') || params.stage.isEmpty())
+		{
+			stage('Nexus') {            
+				bat "curl -v --user admin:123456 --upload-file C:/Users/nmt02/.jenkins/workspace/pipilene_sonar_feature-sonar/build/DevOpsUsach2020-0.0.1.jar http://7fb6-186-79-184-102.ngrok.io/repository/test-repo/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar "            
+			} 
+		}
+
+
+
+
+		/*stage('Test Application') {           
+				    bat "curl -X GET http://localhost:8081/rest/mscovid/test?msg=testing"             
+		}*/
+	}
+	else{
+		println 'verifique hay stages ingresados que no existen.'
+	}
 	
         
         
